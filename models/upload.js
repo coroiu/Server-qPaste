4c2f7abb24838d66458a0ba5be2b2845a14f12f4
Andreas Coroiu <andreas.coroiu@gmail.com>
2014-02-28 15:47:40 +0100

    Fixed Upload model construction bug.
    The default value was static, made them functions instead so that they
    are generated everytime. (This resulted in the same GUID).

--- models/upload.js
+++ models/upload.js
@@ -8,12 +8,16 @@
 		expire.setDate(new Date().getDate() + 1);
 
 		var uploadSchema = mongoose.Schema({
-			uid: {type: String, 'default': uuid.v4()}, // URL to file
+			uid: {type: String, 'default': function() {return uuid.v4();} }, // URL to file
 			resourcepath: String, // Relative Amazon S3 resource path (same as filepath but relative to http://s3.amazonaws.com/qpaste/...)
 			filename: String, // Original filename
 			mimetype: String, // Mime-type
 			uploaded: {type: Boolean, 'default': false}, // Is file uploaded yet?
-			expire: {type: Date, 'default': expire} // Datetime for expiration (auto delete from database)
+			expire: {type: Date, 'default': function() {
+				var expire = new Date();
+				expire.setDate(new Date().getDate() + 1);
+				return expire;
+			}} // Datetime for expiration (auto delete from database)
 		});
 
 		uploadSchema.virtual('url').get(function () {
